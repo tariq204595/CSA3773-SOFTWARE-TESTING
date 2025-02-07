99a4508d2c44b8707081289e2a793c7958d43746
tariq204595 <129891618+tariq204595@users.noreply.github.com>
2025-02-07 13:24:04 +0530

    Update program5.java

--- program5.java
+++ program5.java
@@ -1,33 +1,21 @@
-import java.util.Scanner;
-
-public class program5{
-    public static boolean isPalindrome(int num) {
-        if (num < 0) return false; // Negative numbers are not palindromes
-
-        int originalNum = num;
-        int reverseNum = 0;
-
-        while (num > 0) {
-            int digit = num % 10; // Extract last digit
-            reverseNum = reverseNum * 10 + digit; // Build reverse number
-            num /= 10; // Remove last digit
-        }
-
-        return originalNum == reverseNum;
-    }
-
+import java.util.*;
+class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter a number: ");
-        int userInput = scanner.nextInt();
-        
-        if (isPalindrome(userInput)) {
-            System.out.println(userInput + " is a Palindrome");
-        } else {
-            System.out.println(userInput + " is NOT a Palindrome");
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int rev = 0;
+        int num = n;
 
-        scanner.close();
+        while (num != 0) {
+            int dig = num % 10;
+            rev = rev * 10 + dig;
+            num /= 10;
+        }
+        if (rev == n) { 
+            System.out.println(n + " is a palindrome.");
+        } else {
+            System.out.println(n + " is not a palindrome.");
+        }
     }
 }
